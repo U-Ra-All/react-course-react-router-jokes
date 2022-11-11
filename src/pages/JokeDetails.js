981a89c67dfe23e6de554b40df70353196259c07
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-11-11 09:32:37 +0200

    13-Работаем с Вложенными Маршрутами

--- src/pages/JokeDetails.js
+++ src/pages/JokeDetails.js
@@ -1,8 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const JokeDetails = () => {
   const params = useParams();
-  return <h1>Joke Details Page {params.jokeId}</h1>;
+  return (
+    <Fragment>
+      <h1>Joke Details Page {params.jokeId}</h1>
+      <Route path='/jokes/:jokeId/comments'>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default JokeDetails;
