66fbca85955f613fb74011032ace78ff7c67dbbf
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-11-12 09:07:00 +0200

    18-Программная Имплементация Навигации

--- src/pages/AddJoke.js
+++ src/pages/AddJoke.js
@@ -1,8 +1,13 @@
 import JokeForm from '../components/jokes/JokeForm';
+import { useHistory } from 'react-router-dom';
 
 const AddJoke = () => {
+  const history = useHistory();
+
   const addJokeHandler = (jokeData) => {
     console.log(jokeData);
+
+    history.push('/jokes');
   };
 
   return <JokeForm onAddJoke={addJokeHandler} />;
