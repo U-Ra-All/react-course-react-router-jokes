d8f374c65a67a45697cf4f63945dd0910bb9fb7d
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-11-12 12:51:56 +0200

    20-Работа с Параметрами Запроса

--- src/components/jokes/JokeList.js
+++ src/components/jokes/JokeList.js
@@ -1,13 +1,41 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import JokeItem from './JokeItem';
 import styles from './JokeList.module.css';
 
+const sortJokes = (jokes, isAscending) => {
+  return jokes.sort((joke1, joke2) => {
+    if (isAscending) {
+      return joke1.id > joke2.id ? 1 : -1;
+    } else {
+      return joke1.id < joke2.id ? 1 : -1;
+    }
+  });
+};
+
 const JokeList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const sortingOrder = queryParams.get('sort');
+  const isSortingAscending = sortingOrder === 'asc';
+  const sortedJokes = sortJokes(props.jokes, isSortingAscending);
+
+  const toggleSortingHandler = () => {
+    history.push('/jokes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  };
+
   return (
     <Fragment>
+      <div className={styles.sort}>
+        <button onClick={toggleSortingHandler}>
+          Sort Jokes {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={styles.list}>
-        {props.jokes.map((joke) => (
+        {sortedJokes.map((joke) => (
           <JokeItem
             key={joke.id}
             id={joke.id}
