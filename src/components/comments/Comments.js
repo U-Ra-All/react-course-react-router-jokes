9aba9d2d8b1d6d6f7cfeb2274207321c3538e8f3
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-11-15 15:56:55 +0200

    24-Добавляем Новую Фичу

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,56 @@
-import { useState } from 'react';
-
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 import styles from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getComments } from '../../utils/firebase-api';
+import Loader from '../UI/Loader';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { jokeId } = params;
+
+  const {
+    sendHttpRequest,
+    status,
+    data: loadedComments,
+  } = useHttp(getComments);
+
+  useEffect(() => {
+    sendHttpRequest(jokeId);
+  }, [jokeId, sendHttpRequest]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const commentAddedHandler = useCallback(() => {
+    sendHttpRequest(jokeId);
+  }, [jokeId, sendHttpRequest]);
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <Loader />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === 'completed' &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className='centered'>This joke doesn't have comments yet</p>;
+  }
 
   return (
     <section className={styles.comments}>
@@ -18,8 +60,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          jokeId={params.jokeId}
+          onCommentAdded={commentAddedHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,16 +1,40 @@
-import { useRef } from 'react';
-
+import { useRef, useEffect } from 'react';
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../utils/firebase-api';
 import styles from './NewCommentForm.module.css';
+import Loader from '../UI/Loader';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
+  const { sendHttpRequest, status, error } = useHttp(addComment);
+
+  const { onCommentAdded } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onCommentAdded();
+    }
+  }, [status, error, onCommentAdded]);
+
   const submitFormHandler = (event) => {
     event.preventDefault();
+
+    const enteredText = commentTextRef.current.value;
+
+    sendHttpRequest({
+      commentData: { text: enteredText },
+      jokeId: props.jokeId,
+    });
   };
 
   return (
     <form className={styles.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className='centered'>
+          <Loader />
+        </div>
+      )}
       <div className={styles.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
